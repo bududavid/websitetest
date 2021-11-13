c58ddb962a8f3954af43a4526f61a39404418390
bududavid <93273247+bududavid@users.noreply.github.com>
2021-11-13 13:41:15 +0200

    website

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict"
+
+console.log('Я учу JS')
+
+let userName = 'David',
+	age = 15
+	age = 16
+
+const name = 'Budu'
+
+console.log(name)
+console.log(userName)
+console.log(age)
+
+let test = false
+if (test) {
+	console.log('1');
+} else {
+	console.log('0');
+}
+
+const test1 = 988888888888888888888888888888888888888888888888888888n;
+console.log(test1)
